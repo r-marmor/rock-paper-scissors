8f6418313ed092054fd3ca221be33acf6a83d976
106Manner <raphael.marmor@gmail.com>
2022-12-07 16:29:07 +0100

    add an ui to the game

--- script.js
+++ script.js
@@ -5,79 +5,67 @@ const getComputerChoice = () => {
     return options[Math.floor(Math.random() * options.length)]
 }
 
-// Ask player for a choice included in const options
-const getPlayerChoice = () => {
-    let userInput = false;
-    while (userInput == false) {
-        const choice = prompt("Rock, paper or scissors?");
-        const choiceToLower = choice.toLowerCase();
-        if (options.includes(choiceToLower)) {
-            userInput = true;
-            return choiceToLower;
-        }
-    }
+// Creating an UI
+let playerScore = 0;
+let cpuScore = 0;
+
+const btn = document.querySelectorAll('.btn button');
+const msg = document.querySelector('.msg');
+const score = document.querySelector('.score');
+const finalmsg = document.getElementById('finalmsg');
+const resetBtn = document.querySelector('#reset');
+
+// Reloading page for new game
+resetBtn.addEventListener('click', () => location.reload());
+
+function disableButtons() {
+    btn.forEach(elem => {
+        elem.disabled = true;
+    })
 }
 
-
-// const checkWinner = (playerSelection, cpuSelection) => {
-//     if (playerSelection == cpuSelection)
-//         return "Tie"
-//     else if (
-//         (playerSelection == "rock" && cpuSelection == "scissors") ||
-//         (playerSelection == "paper" && cpuSelection == "rock") ||
-//         (playerSelection == "scissors" && cpuSelection == "paper")
-        
-//     ){
-//         return `Player won`
-//     } else {
-//         return `Computer won`
-//     }  
-// }
-
-// simulate a single round
 const playRound = (playerSelection, cpuSelection) => {
+    let result = "";
+
     if (playerSelection == cpuSelection)
-        return "it's a tie!"
+        result = `You both choose ${playerSelection}, it's a tie!`
     else if (
         (playerSelection == "rock" && cpuSelection == "scissors") ||
         (playerSelection == "paper" && cpuSelection == "rock") ||
-        (playerSelection == "scissors" && cpuSelection == "paper")
-        
+        (playerSelection == "scissors" && cpuSelection == "paper")   
     ){
-        return `Player wins this round! ${playerSelection} beats ${cpuSelection}`
+        playerScore++
+        result = `Player wins this round! ${playerSelection} beats ${cpuSelection}`
+        score.innerText = `Player ${playerScore} - ${cpuScore} Computer`;
+
+        if (playerScore == 4) {
+            msg.innerText = "One more win to win the match !!"
+        }
+        else if (playerScore == 5) {
+                result = "You won the game ! Click 'Play again' for a rematch'"
+                disableButtons();
+        }
+
     } else {
-        return `Computer wins this round! ${cpuSelection} beats ${playerSelection}`
+        cpuScore++
+        result = `Computer wins this round! ${cpuSelection} beats ${playerSelection}`
+        score.innerText = `Player ${playerScore} - ${cpuScore} Computer`;
+        
+        if (CpuScore == 4) {
+            msg.innerText = "One more win to win the match !!"
+        }
+        else if (cpuScore == 5) {
+                result = "Computer won the game ! Click 'Play again' for a rematch'"
+                disableButtons();
     }  
 }
 
+finalmsg.innerText = result;
 
-// plays 5 round, keep track of points from players
-const game = () => {
-    let playerScore = 0;
-    let CpuScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = getPlayerChoice();
-        let cpuSelection = getComputerChoice();
-        playRound(playerSelection, cpuSelection)
-        if (playRound(playerSelection, cpuSelection) == `Player wins this round! ${playerSelection} beats ${cpuSelection}`) {
-            playerScore++
-            console.log(`Player wins this round! ${playerSelection} beats ${cpuSelection}`)
-            console.log('--------------------------')
-        }
-        else if (playRound(playerSelection, cpuSelection) == `Computer wins this round! ${cpuSelection} beats ${playerSelection}`) {
-            CpuScore++ 
-            console.log(`Computer wins this round! ${cpuSelection} beats ${playerSelection}`)
-            console.log('--------------------------')
-        }   
-    }
-    
-    console.log(`Final score is Player ${playerScore} - ${CpuScore} Computer`)
-    console.log('==========================')
-    if (playerScore > CpuScore)
-        return "Congratulations, you won the match!"
-    else if (playerScore < CpuScore)
-        return "Ewwww, it seems that the CPU won the match. Better luck next time!"
-    return "The match ends up on a tie!"
 }
 
-console.log(game())
+btn.forEach(button => {
+    button.addEventListener('click', () => {
+        playRound(button.innerText.toLowerCase(), getComputerChoice())
+    })
+})
